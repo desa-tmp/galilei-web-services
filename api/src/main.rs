a6982086f3bfbdaba46e24e8aac99be5f3805c61
Luca De Santis <lucaroma2705@gmail.com>
2024-01-22 17:18:40 +0100

    Add simple actix http server

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,34 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
 use api;
 
-fn main() {
-  let a = 2;
-  let b = 2;
+const fn default_i32<const V: i32>() -> i32 {
+  V
+}
 
-  println!("{a} + {b} = {}", api::add(a, b));
+#[derive(Debug, Deserialize)]
+struct Addition {
+  #[serde(default = "default_i32::<2>")]
+  left: i32,
+  #[serde(default = "default_i32::<2>")]
+  right: i32,
+}
+
+#[get("/")]
+async fn hello_world() -> impl Responder {
+  HttpResponse::Ok().body("Hello, World!")
+}
+
+#[get("/add")]
+async fn add(web::Query(Addition { left, right }): web::Query<Addition>) -> impl Responder {
+  HttpResponse::Ok().body(format!("{left} + {right} = {}", api::add(left, right)))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+  HttpServer::new(|| App::new().service(hello_world).service(add))
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
