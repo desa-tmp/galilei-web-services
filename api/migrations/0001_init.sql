a19038adaf3396e497c23e18649267bf611ac4d0
Luca De Santis <lucaroma2705@gmail.com>
2024-04-15 19:00:37 +0200

    add type check to planet capacity
    capacity must be grater or equal to 0

---
+++ api/migrations/0001_init.sql
@@ -0,0 +1,41 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS users (
+  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+  expires TIMESTAMP NOT NULL,
+  user_id UUID NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS galaxies (
+  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  user_id UUID NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  CONSTRAINT galaxy_name_user UNIQUE (name, user_id) -- unique galaxy name for a user
+);
+
+CREATE TABLE IF NOT EXISTS stars (
+  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  nebula TEXT NOT NULL,
+  galaxy_id UUID NOT NULL,
+  FOREIGN KEY (galaxy_id) REFERENCES galaxies(id) ON DELETE CASCADE,
+  CONSTRAINT star_name_galaxy UNIQUE (name, galaxy_id) -- unique star name inside a galaxy
+);
+
+CREATE TABLE IF NOT EXISTS planets (
+  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  capacity INT NOT NULL CHECK(capacity >= 0), -- unit size is MB
+  star_id UUID, -- a planet may not have a star associated with it
+  galaxy_id UUID NOT NULL,
+  FOREIGN KEY (star_id) REFERENCES stars(id) ON DELETE SET NULL,
+  FOREIGN KEY (galaxy_id) REFERENCES galaxies(id) ON DELETE CASCADE,
+  CONSTRAINT planet_name_galaxy UNIQUE (name, galaxy_id) -- unique planet name inside a galaxy
+);
