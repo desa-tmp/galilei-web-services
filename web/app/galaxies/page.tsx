d8e4a7a8824478d9928a018dc8ca184fabfbb674
Luca De Santis <lucaroma2705@gmail.com>
2024-04-30 23:07:33 +0200

    add basic galaxies list page

--- web/app/galaxies/page.tsx
+++ web/app/galaxies/page.tsx
@@ -1,3 +1,14 @@
-export default function Galaxies() {
-  return <h1>User Galaxies</h1>;
+import { fetchApi } from "@/lib/api";
+import { Galaxy } from "@/lib/schema";
+
+export default async function Galaxies() {
+  const galaxies = (await (await fetchApi("/galaxies")).json()) as Galaxy[];
+
+  return (
+    <div className="flex flex-col gap-4">
+      {galaxies.map(({ id, name }) => (
+        <div key={id}>{name}</div>
+      ))}
+    </div>
+  );
 }
