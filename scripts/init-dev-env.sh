9575abf68a3eb3fad469abb97254765f2eb101ec
Luca De Santis <96928386+fvckDesa@users.noreply.github.com>
2024-05-12 21:35:38 +0000

    add postgres trust connection permitions

--- scripts/init-dev-env.sh
+++ scripts/init-dev-env.sh
@@ -8,6 +8,12 @@ cargo install sqlx-cli
 # create .env file
 cp .env.example .env
 
+# add postgres connection trust permitions for IPv4 and IPv6
+IPv4_TRUST_PERM="host    all             all             127.0.0.1/32            trust"
+IPv6_TRUST_PERM="host    all             all             ::1/128                 trust"
+echo $IPv4_TRUST_PERM | sudo tee -a /etc/postgresql/16/main/pg_hba.conf
+echo $IPv6_TRUST_PERM | sudo tee -a /etc/postgresql/16/main/pg_hba.conf
+
 # start postgres service
 sudo service postgresql start
 
